d878fda6793ed497e4e02a47caf00b493e5dae6c
e3pax <pax@archpax.localdomain>
2021-03-09 20:59:28 +0000

    Allowed secureCheck function to detect if a website is not reachable

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 # Webler, a command line program that gives info about a url
 
-import requests, sys, signal
+import requests, sys
 
 url = sys.argv[1]
 
-try:
-    reqUrl = requests.get("https://" + str(url))
-    print("Url is secure")
-except:
-    print("Url is insecure")
+def secureCheck():
+    try:
+        requests.get("https://" + str(url))
+        print("Url is secure")
+    except:
+        try:
+            requests.get("http://" + str(url))
+            print("Url is insecure")
+        except:
+            print("Url is unreachable")
 
-# TODO: Make function time out after 10s and print("Url is unreachable, possibly insecure"), a scenario happens like this with go.com
-# Allow full url to be entered
+secureCheck()
+
+
+# TODO: Allow full url to be entered
