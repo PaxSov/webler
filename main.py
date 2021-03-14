b0b0fbff24fe6320c3352dc8c3137444acc976ed
e3pax <pax@archpax.localdomain>
2021-03-14 21:14:17 +0000

    Did first options test

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python3
 # Webler, a command line program that gives info about a url
+# Branch: options-test
 
-import requests, sys
 
-url = sys.argv[1]
+import requests, sys, re
 
-def secureCheck():
-    try:
-        requests.get("https://" + str(url))
-        print("Url is secure")
-    except:
+url = sys.argv[2]
+opt = sys.argv[1]
+
+def secureCheck(optSelect):
+    if optSelect == 0:
         try:
-            requests.get("http://" + str(url))
-            print("Url is insecure")
+            requests.get("https://" + str(url))
+            print("Url is secure")
         except:
-            print("Url is unreachable")
+            try:
+                requests.get("http://" + str(url))
+                print("Url is insecure")
+            except:
+                print("Url is unreachable")
+    elif optSelect == 1:
+        try:
+            requests.get("https://" + str(url))
+            print("Url is secure")
+        except:
+            try:
+                requests.get("http://" + str(url))
+                print("Url is insecure")
+            except:
+                print("Url is unreachable")
+    elif optSelect == 2:
+        try:
+            requests.get(str(url))
+            print("Url is secure")
+        except:
+            try:
+                requests.get("http://" + str(url))
+                print("Url is insecure")
+            except:
+                print("Url is unreachable")
 
-secureCheck()
+
+
+secureCheck(optSelect)
 
 
 # TODO: Allow full url to be entered
+# TODO: Option 0 = google.com entered | Option 1: https://google.com entered | Option 2: www.https://google.com entered
+# The different functions are for then all to work
+# Add help option
+# Use argparse
