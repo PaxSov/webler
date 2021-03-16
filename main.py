6e171f20a7b0c40a040530884a427e49d651c6f2
e3pax <e3pax@protonmail.ch>
2021-03-16 07:34:28 +0000

    Learning to ammend and rebase | Fixed 2 spelling errors in helpMessage()

--- main.py
+++ main.py
@@ -5,18 +5,37 @@ import requests, sys
 
 url = sys.argv[1]
 
+def helpMessage():
+    print('Webler, a command line program that gives info about a url')
+    print('Usage: webler {url}')
+    print('This program uses the MIT license | THERE IS NO WARREATY')
+
+try:
+    url = sys.argv[1]
+except:
+    helpMessage()
+
 def secureCheck():
-    try:
-        requests.get("https://" + str(url))
-        print("Url is secure")
-    except:
         try:
-            requests.get("http://" + str(url))
-            print("Url is insecure")
+            re.sub('https://','',url)  
+            re.sub('http://','',url)   
+            re.sub('www.','',url)
+            requests.get(str(url))
+            print("Url is secure")
         except:
-            print("Url is unreachable")
+            try:
+                requests.get("http://" + str(url))
+                print("Url is insecure")
+            except:
+                print("Url is unreachable")
 
-secureCheck()
 
 
-# TODO: Allow full url to be entered
+try:
+    secureCheck()
+except:
+    pass
+
+
+# Add help option
+# Use argparse
