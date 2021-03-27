5c3149492edab7fe331cbff83d58f441bde1df7d
e3pax <e3pax@protonmail.ch>
2021-03-27 15:25:15 +0000

    Added a beta for exporting output to text files

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # Webler, a command line program that gives info about a url
 
-import requests, sys
+import requests, sys, re
 
 url = sys.argv[1]
+isSecure = 2 # 0 = url secure | 1 = not secure | 2 = unreachable
 
 def helpMessage():
     print('Webler, a command line program that gives info about a url')
     print('Usage: webler {url}')
-    print('This program uses the MIT license | THERE IS NO WARREATY')
+    print('This program uses the MIT license | THERE IS NO WARRENTY')
 
 try:
     url = sys.argv[1]
@@ -22,17 +23,29 @@ def secureCheck():
             re.sub('www.','',url)
             requests.get(str(url))
             print("Url is secure")
+            isSecure = 0
         except:
             try:
                 requests.get("http://" + str(url))
                 print("Url is insecure")
+                isSecure = 1
             except:
-                print("Url is unreachable")
+                print("Url is unreachable | Program Aborting")
+                isSecure = 0
+                stop()
 
+def writeTxt(isSecure):
+    if isSecure == 0:
+        open('output.txt', 'a').write('https = true')
+    elif isSecure == 1:
+        open('output.txt', 'a').write('https = false')
+    else:
+        open('output.txt', 'a').write('https = N/A')
 
 
 try:
     secureCheck()
+    writeTxt(isSecure)
 except:
     pass
 
